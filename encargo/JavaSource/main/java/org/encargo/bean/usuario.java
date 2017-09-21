c73dd6244e65e994309222b0591ce9be1def1f64
jalozanot2 <jalozanot2@gmail.com>
2017-09-20 20:57:25 -0500

    actualizacion del archivo de usuario.java

--- encargo/JavaSource/main/java/org/encargo/bean/usuario.java
+++ encargo/JavaSource/main/java/org/encargo/bean/usuario.java
@@ -1,5 +1,141 @@
 package org.encargo.bean;
 
-public class usuario {
+import java.util.Date;
 
+public class usuario {
+	
+	private Integer id;
+	private String nombre;
+	private String password;
+	private Integer estado;
+	private Date fechaModificacion;
+	private Date fechaCreacion;
+	private Integer creacionUsuario;
+	private Integer updateUsuario;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Integer getEstado() {
+		return estado;
+	}
+	public void setEstado(Integer estado) {
+		this.estado = estado;
+	}
+	
+	public Date getFechaModificacion() {
+		return fechaModificacion;
+	}
+	public void setFechaModificacion(Date fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	
+	public Integer getCreacionUsuario() {
+		return creacionUsuario;
+	}
+	public void setCreacionUsuario(Integer creacionUsuario) {
+		this.creacionUsuario = creacionUsuario;
+	}
+	
+	public Integer getUpdateUsuario() {
+		return updateUsuario;
+	}
+	public void setUpdateUsuario(Integer updateUsuario) {
+		this.updateUsuario = updateUsuario;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((creacionUsuario == null) ? 0 : creacionUsuario.hashCode());
+		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
+		result = prime * result + ((fechaCreacion == null) ? 0 : fechaCreacion.hashCode());
+		result = prime * result + ((fechaModificacion == null) ? 0 : fechaModificacion.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((updateUsuario == null) ? 0 : updateUsuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		usuario other = (usuario) obj;
+		if (creacionUsuario == null) {
+			if (other.creacionUsuario != null)
+				return false;
+		} else if (!creacionUsuario.equals(other.creacionUsuario))
+			return false;
+		if (estado == null) {
+			if (other.estado != null)
+				return false;
+		} else if (!estado.equals(other.estado))
+			return false;
+		if (fechaCreacion == null) {
+			if (other.fechaCreacion != null)
+				return false;
+		} else if (!fechaCreacion.equals(other.fechaCreacion))
+			return false;
+		if (fechaModificacion == null) {
+			if (other.fechaModificacion != null)
+				return false;
+		} else if (!fechaModificacion.equals(other.fechaModificacion))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (updateUsuario == null) {
+			if (other.updateUsuario != null)
+				return false;
+		} else if (!updateUsuario.equals(other.updateUsuario))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 }
